b5a2ccde5d9fe81192a16ebff78ff534573fde81
Kirill <kir17bul@gmail.com>
2018-10-22 20:48:39 +0300

    Task09

--- Bulygin_Kirill_Task09/Task1/Program.cs
+++ Bulygin_Kirill_Task09/Task1/Program.cs
@@ -8,65 +8,31 @@ namespace Task1
 {
     class Program
     {
-        //static int RemoveEachSecondItem(LinkedList<int> list)
-        //{
-        //    if (list.Count == 0)
-        //        return -1;
-        //    var node = list.First.Next ?? list.First;
-        //    while (list.Count != 1)
-        //    {
-        //        var next = node.Next ?? list.First;
-        //        list.Remove(node);
-        //        node = next?.Next ?? list.First;
-        //    }
-
-        //    return list.First.Value;
-        //}
 
         static int RemoveEachSecondItem(ICollection<int> list)
         {
             if (list.Count < 2)
                 return -1;
-            var e = list.GetEnumerator();
-            int c = list.Count;
-            e.MoveNext();
-            for (;list.Count > 1; )
+            var isEven = false;
+            while (list.Count > 1)
             {
-                List<int> tmp = new List<int>();
-
-                c = list.Count;
-                while (e.MoveNext())
+                var evens = new List<int>();
+                foreach (var i in list)
                 {
-                    tmp.Add(e.Current);
-                    e.MoveNext();
+                    if (isEven)
+                    {
+                        evens.Add(i);
+                    }
+                    isEven = !isEven;
                 }
-                
-                
-                foreach (var i in tmp)
-                {
-                    list.Remove(i);
 
-                }
-                e = list.GetEnumerator();
-                if (c % 2 == 0)
+                foreach (var even in evens)
                 {
-                    e.MoveNext();
+                    list.Remove(even);
                 }
             }
             return list.First();
         }
-
-        //static int RemoveEachSecondItem(List<int> list)
-        //{
-        //    if (list.Count == 0)
-        //        return -1;
-        //    for (int i = 1; list.Count > 1; i = (i + 1) % list.Count)
-        //    {
-        //        list.RemoveAt(i);
-        //    }
-
-        //    return list[0];
-        //}
         
         static void Main(string[] args)
         {
